23fabf8682fd38feb9112410db88ec45487791ea
LtCocoa <dea95@ya.ru>
2026-07-04 16:53:06 +0300

    added vite-env

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue';
 import { router } from './router';
 
+import './main.css';
+
 const pinia = createPinia();
 
 createApp(App)
   .use(pinia)
-  .use(router);
+  .use(router)
+  .mount('#app');
